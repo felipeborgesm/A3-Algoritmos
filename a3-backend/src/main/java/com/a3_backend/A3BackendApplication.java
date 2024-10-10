8255bbec52d116a7c1a3774ee0083aa2047b8b23
felipeborgesm <cafeborges@outlook.com>
2024-10-09 22:02:55 -0300

    initial files created

---
+++ a3-backend/src/main/java/com/a3_backend/A3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.a3_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A3BackendApplication.class, args);
+	}
+
+}
