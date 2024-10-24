6ace8c75c78b391ce01e19f657d9ed31d78bef79
felipeborgesm <cafeborges@outlook.com>
2024-10-23 22:39:52 -0300

    corrigindo docker

--- a3-backend/src/main/java/com/a3_backend/A3BackendApplication.java
+++ a3-backend/src/main/java/com/a3_backend/A3BackendApplication.java
@@ -1,9 +1,21 @@
 package com.a3_backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(
+		info =
+		@Info(
+				title = "API Sistema Bancario",
+				version = "2.0",
+				description =
+						"Essa API simula um sistema bancário, permitindo a criação de um usuario, que pode criar uma conta"
+								+ " e que pode fazer transações para outras contas de outros usuarios"))
 public class A3BackendApplication {
 
 	public static void main(String[] args) {
